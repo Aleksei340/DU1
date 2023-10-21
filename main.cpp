b474eb7f4fc301f5fcb55ca047ded29710eefa40
Aleksei340 <aleshaburkov@gmail.com>
2023-10-21 15:03:13 +0200

    Added "click" struct and entering data from terminal for it.

--- main.cpp
+++ main.cpp
@@ -7,20 +7,30 @@ const int MAXF = 10;
 const char *farby[MAXF] = {"red", "green", "blue", "black", "gray",
                            "yellow", "brown", "orange", "purple", "aqua"};
 
-struct okno {
+struct window {
     int x1, y1, x2, y2, f, clicks = 0;
+};
+
+struct click {
+    int x, y;
 };
 
 int main() {
     int o, k;
     cin >> o >> k;
     int x1, y1, x2, y2, f;
-    vector<okno> okna;
+    vector<window> windows;
+    vector<click> clicks;
     for (int i = 0; i < o; i++) {
-        okno temp;
+        window temp;
         cin >> temp.x1 >> temp.y1
             >> temp.x2 >> temp.y2 >> temp.f;
-        okna.push_back(temp);
+        windows.push_back(temp);
+    }
+    for (int i = 0; i < k; i++) {
+        click temp;
+        cin >> temp.x >> temp.y;
+        clicks.push_back(temp);
     }
     return 0;
 }
